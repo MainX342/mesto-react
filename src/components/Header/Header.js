6daadfdc8c53e28f3f430ffafa2a8689c17d159d
MainX342 <vladimirtitovperm@gmail.com>
2023-06-27 11:14:28 +0500

    Updates

--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import logo from '../../images/logo.svg'
-
-export default function Header() {
-  return (
-    <header className="header page__header">
-      <img src={logo} alt="Логотип" className="header__logo" />
-    </header>
-  )
-}
